6e5ba9a7b7f61de359459bb6bc78c1a8f56181fd
ChhanunHean <chhanunhean@gmail.com>
2026-07-19 13:53:51 +0700

    fix error target to match with backend

--- recon_mapper/lib/main.dart
+++ recon_mapper/lib/main.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
 import 'theme/app_theme.dart';
+import 'models/target.dart';
 
 void main() {
+  // TEMPORARY TEST — remove this block once confirmed working
+  final testJson = {
+    "id": 1,
+    "domain": "google.com",
+    "ip": "142.250.185.46",
+    "ping_ms": 42.8,
+    "risk_score": 76,
+    "risk_level": "Medium",
+    "last_scanned": "2026-07-17T17:33:04.199059"
+  };
+
+  final target = Target.fromJson(testJson);
+
+  print("id: ${target.id}");
+  print("domain: ${target.domain}");
+  print("ip: ${target.ip}");
+  print("ping: ${target.ping}");
+  print("riskScore: ${target.riskScore}");
+  print("riskLevel: ${target.riskLevel}");
+  print("lastScanned: ${target.lastScanned}");
+  // END TEMPORARY TEST
+
   runApp(const ReconMapperApp());
 }
+
 class ReconMapperApp extends StatelessWidget {
   const ReconMapperApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,7 +45,6 @@ class ReconMapperApp extends StatelessWidget {
 
 class PlaceholderHome extends StatelessWidget {
   const PlaceholderHome({super.key});
-
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
